bd41b962df5823aa9a7e0bc0befcac7e1dce71ff
Andrey Kondratyuk <andrey.vw@gmail.com>
2021-06-13 18:03:34 -0400

    different spelling of classnames

--- src/components/TagsThumbnail.js
+++ src/components/TagsThumbnail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import classNames from 'classNames';
+import classNames from 'classnames';
 
 const Container = styled.div`
   width: 100%;
